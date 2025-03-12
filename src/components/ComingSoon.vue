45098701c17bff7ebb741b245ef1f7e9498d5ff9
Coby Yates <cobyyates@Cobys-MacBook-Pro.local>
2025-03-12 06:52:24 -0600

    initial commit

---
+++ src/components/ComingSoon.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="align-centerfill-height mx-auto text-center">
+      <v-row>
+        <v-col>
+          <v-img
+            class="mb-4"
+            height="400"
+            src="@/assets/overlander-classifieds-coming-soon.png"
+          />
+          <h1>Future home of Overlander Classifieds</h1>
+          <p>
+            We are currently building something powerful. Follow our social
+            media for future updates
+          </p>
+            <v-btn
+              icon
+              class="mb-2 elevation-0"
+              color="transparent"
+              size="x-large"
+              href="https://instagram.com/overlanderclassifieds"
+              target="_blank"
+            >
+              <v-icon color="purple" size="x-large">mdi-instagram</v-icon>
+            </v-btn>
+            <v-btn
+              icon
+              class="mb-2 elevation-0 mx-4"
+              color="transparent"
+              size="x-large"
+              href="https://www.facebook.com/overlanderclassifieds"
+              target="_blank"
+            >
+              <v-icon color="primary" size="x-large">mdi-facebook</v-icon>
+            </v-btn>
+            <v-btn
+              icon
+              class="mb-2 elevation-0"
+              color="transparent"
+              size="x-large"
+              href="https://www.youtube.com/@OverlanderClassifieds"
+              target="_blank"
+            >
+              <v-icon color="red" size="x-large">mdi-youtube</v-icon>
+            </v-btn>
+            <v-btn
+              icon
+              class="mb-2 elevation-0"
+              color="transparent"
+              size="x-large"
+              href="https://www.linkedin.com/company/overlander-classifieds"
+              target="_blank"
+            >
+              <v-icon color="primary" size="x-large">mdi-linkedin</v-icon>
+            </v-btn>
+        </v-col>
+      </v-row>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup>
+//
+</script>
---
+++ src/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <ComingSoon />
+</template>
+
+<script setup>
+  //
+</script>
